0149f0b7b5033018040f672e7f24a94879307830
Uwe Helm <uwe.helm@gmail.com>
2015-03-11 13:25:08 -0700

    fixup

--- arch_dev/modules/arch_dev/files/site.pp
+++ arch_dev/modules/arch_dev/files/site.pp
@@ -10,28 +10,6 @@
 # sysctl::value needs the default exec path set; lets set it to root's default
 Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/core_perl' }
 
-# for the iptables and firewall stuff
-resources { "firewall":
-  purge => true,
-}
-
-# this ensures that the _pre and _post classes are run in that order, to make sure we don't get locked out of the box
-Firewall {
-  before  => Class['arch_dev::firewall_post'],
-  require => Class['arch_dev::firewall_pre'],
-}
-
-# this sets up default allow rules so we don't gronk the box
-class {'arch_dev::firewall_pre':
-  require => File['/etc/conf.d/iptables'],
-}
-
-# this sets up a default drop rule and makes sure it's last
-class {'arch_dev::firewall_post' : }
-
-# this installs the require packages (iptables)
-class {'firewall': }
-
 node default {
   include arch_dev
 }
